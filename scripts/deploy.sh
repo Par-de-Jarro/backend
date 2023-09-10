f18dbe9e481db5466be01ee8380df071f4c3b5e3
ekarani <ekarani.silvestre@ccc.ufcg.edu.br>
2023-09-10 18:22:33 -0300

    configure Deploy via actions

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#docker stop app_server database
+#docker rm app_server database
+
+STR=`docker ps`
+SUB1='server_app'
+SUB2='database'
+if [[ "$STR" == *"$SUB1"* ]]; then
+	docker stop ${SUB1}
+	docker rm ${SUB1}	
+fi
+
+if [[ "$STR" == *"$SUB2"* ]]; then
+        docker stop ${SUB2}
+        docker rm ${SUB2}
+fi
+
+docker-compose -f /home/ubuntu/production_backend/docker-compose.yml up
+
