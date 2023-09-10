de383c3faa3ea768845d3dbe061b074d102ae360
ricardoadley <ricardo.sena@ccc.ufcg.edu.br>
2023-09-10 16:49:15 -0300

    fix: try to fix make_user

--- tests/factories.py
+++ tests/factories.py
@@ -1,5 +1,7 @@
 import pytest
 
+from app.user.schemas.user import UserCreate
+
 
 @pytest.fixture
 def make_todo():
@@ -8,4 +10,12 @@ def make_todo():
 
 @pytest.fixture
 def make_user():
-    ...
+    user = UserCreate()
+    user.name = "Ricardinho"
+    user.email = "teste@email.com"
+    user.cellphone = "11999999999"
+    user.document_id = "12345678901"
+    user.birthdate = "1990-04-13"
+    user.course = "Ciência da Computação"
+    user.bio = "Teste"
+    return user
