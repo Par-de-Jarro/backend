af12f0dea83863abdf33c6247c5c220e07e54cbc
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-09-16 11:37:46 -0300

    feat: add mock id_user on user tests

--- tests/test_user_endpoint.py
+++ tests/test_user_endpoint.py
@@ -1,13 +1,27 @@
 import json
+from uuid import UUID
 
 import pytest
 
+from app.api.deps import get_id_user_by_auth_token
+from app.main import app
+
 from .base_client import BaseClient
+
+
+async def override_id_user(id_user: UUID):
+    return id_user
 
 
 class UserClient(BaseClient):
     def __init__(self, client):
         super().__init__(client, endpoint_path="user")
+
+    def update(self, update):
+        return self.client.put(f"/{self.path}/", data=update, headers=self.headers)
+
+    def delete(self):
+        return self.client.delete(f"/{self.path}/", headers=self.headers)
 
 
 @pytest.fixture
@@ -42,31 +56,34 @@ def test_create_user(user_client):
     "field,expected_field",
     [("name", "Novo nome"), ("cellphone", "11888888888"), ("document_id", "12312312312")],
 )
-def test_update_user(user, session, user_client, field, expected_field):
+def test_update_user(fastapi_dep, user, session, user_client, field, expected_field):
     session.add(user)
     session.commit()
 
-    data = {field: expected_field}
+    with fastapi_dep(app).override({get_id_user_by_auth_token: user.id_user}):
+        data = {field: expected_field}
 
-    response = user_client.update(id=user.id_user, update=json.dumps(data))
-    assert response.status_code == 200
-    assert response.json()[field] == expected_field
+        response = user_client.update(update=json.dumps(data))
+        assert response.status_code == 200
+        assert response.json()[field] == expected_field
 
 
-def test_delete_user(user, session, user_client):
+def test_delete_user(user, fastapi_dep, session, user_client):
     session.add(user)
     session.commit()
 
-    user_client.delete(id=user.id_user)
-    response = user_client.get_by_id(id=user.id_user)
-    assert response.status_code == 404
-    assert response.json()["detail"] == "User not found"
+    with fastapi_dep(app).override({get_id_user_by_auth_token: user.id_user}):
+        user_client.delete()
+        response = user_client.get_by_id(id=user.id_user)
+        assert response.status_code == 404
+        assert response.json()["detail"] == "User not found"
 
 
 def test_get_user_by_id(user, session, user_client):
     session.add(user)
     session.commit()
     response = user_client.get_by_id(id=user.id_user)
+
     assert response.status_code == 200
     assert response.json()["name"] == "Ricardinho"
     assert response.json()["course"] == "Ciência da Computação"
