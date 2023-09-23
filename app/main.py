62cd98868ae7f19ec0afa6b4c3b89cb0b5fee6de
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-09-23 10:37:18 -0300

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from json import JSONEncoder
 from uuid import UUID
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api.api_v1.api import api_router as v1_api_router
 
@@ -23,5 +24,14 @@ JSONEncoder.default = custom_json_encoder
 app = FastAPI(
     title="Par de Jarro API",
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(v1_api_router)
