74951bea929d59e00b2ea74b39837d880929af22
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-09-25 12:27:34 -0300

    fix: migration

--- migrations/versions/2d931354c1e0_add_gender_column_to_user_table.py
+++ migrations/versions/2d931354c1e0_add_gender_column_to_user_table.py
@@ -7,18 +7,25 @@ Create Date: 2023-09-24 19:07:04.736380
 """
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy.dialects.postgresql import ENUM
 
 # revision identifiers, used by Alembic.
-revision = '2d931354c1e0'
-down_revision = '74c0e1cecd97'
+revision = "2d931354c1e0"
+down_revision = "74c0e1cecd97"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column('user', sa.Column('gender', sa.Enum("FEMALE", "MALE", "NONBINARY", "UNINFORMED", name="usergender"),
-                                    nullable=False, server_default='UNINFORMED'))
+    enum = ENUM("FEMALE", "MALE", "NONBINARY", "UNINFORMED", name="usergender", create_type=False)
+    enum.create(op.get_bind(), checkfirst=False)
+
+    op.add_column(
+        "user",
+        sa.Column("gender", enum, nullable=True),
+    )
 
 
 def downgrade():
-    op.drop_column('user', 'gender')
+    op.drop_column("user", "gender")
+    ENUM(name="usergender").drop(op.get_bind(), checkfirst=False)
