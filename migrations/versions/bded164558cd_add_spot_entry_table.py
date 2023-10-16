044619dacd12099c6f0a6d7ee8db2921af15e619
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2023-10-16 14:21:16 -0300

    fix: migrations

--- migrations/versions/bded164558cd_add_spot_entry_table.py
+++ migrations/versions/bded164558cd_add_spot_entry_table.py
@@ -1,33 +1,40 @@
 """add spot entry table
 
 Revision ID: bded164558cd
-Revises: 7ff9c245e7c0
+Revises: 917df9194b2a
 Create Date: 2023-10-16 10:15:04.000093
 
 """
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy.dialects.postgresql import UUID, ENUM
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.dialects.postgresql import ENUM
+
 from app.spot.schemas.spot_entry_request import EntryRequestStatus
 
 # revision identifiers, used by Alembic.
-revision = 'bded164558cd'
-down_revision = '7ff9c245e7c0'
+revision = "bded164558cd"
+down_revision = "917df9194b2a"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    enum = ENUM("ACCEPTED", "NOT_ACCEPTED", "REQUEST", name="entryrequeststatus")
+    enum = ENUM("ACCEPTED", "NOT_ACCEPTED", "REQUEST", name="entryrequeststatus", create_type=False)
     enum.create(op.get_bind(), checkfirst=False)
 
     op.create_table(
         "spot_entry_request",
-        sa.Column("id_spot_entry_request", UUID(as_uuid=True), server_default=sa.text('gen_random_uuid()'),
-                  nullable=False, primary_key=True),
+        sa.Column(
+            "id_spot_entry_request",
+            postgresql.UUID(as_uuid=True),
+            server_default=sa.text("gen_random_uuid()"),
+            nullable=False,
+            primary_key=True,
+        ),
         sa.Column("status", enum, default=EntryRequestStatus.REQUEST),
-        sa.Column("id_user", sa.Integer(), sa.ForeignKey("user.id_user"), nullable=False),
-        sa.Column("id_spot", sa.Integer(), sa.ForeignKey("spot.id_spot"), nullable=False),
+        sa.Column("id_user", postgresql.UUID(), sa.ForeignKey("user.id_user"), nullable=False),
+        sa.Column("id_spot", postgresql.UUID(), sa.ForeignKey("spot.id_spot"), nullable=False),
     )
 
 
--- migrations/versions/ed7fb8652d4a_add_spot_user_association_table.py
+++ migrations/versions/ed7fb8652d4a_add_spot_user_association_table.py
@@ -1,32 +1,37 @@
 """Add Spot-User association table
 
 Revision ID: ed7fb8652d4a
-Revises: 917df9194b2a
+Revises: bded164558cd
 Create Date: 2023-09-28 23:18:32.893811
 
 """
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy import UUID, text
+from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = 'ed7fb8652d4a'
-down_revision = '917df9194b2a'
+revision = "ed7fb8652d4a"
+down_revision = "bded164558cd"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.create_table(
-        'spot_user',
-        sa.Column('id_spot_user', UUID(as_uuid=True), server_default=text('gen_random_uuid()'), nullable=False,
-                  primary_key=True),
-        sa.Column('id_user', sa.Integer(), nullable=False, index=True),
-        sa.Column('id_spot', sa.Integer(), nullable=False, index=True),
+        "spot_user",
+        sa.Column(
+            "id_spot_user",
+            postgresql.UUID(as_uuid=True),
+            server_default=sa.text("gen_random_uuid()"),
+            nullable=False,
+            primary_key=True,
+        ),
+        sa.Column("id_user", postgresql.UUID(), nullable=False, index=True),
+        sa.Column("id_spot", postgresql.UUID(), nullable=False, index=True),
     )
     op.create_foreign_key("spot_user_id_user", "spot_user", "user", ["id_user"], ["id_user"])
     op.create_foreign_key("spot_user_id_spot", "spot_user", "spot", ["id_spot"], ["id_spot"])
 
 
 def downgrade():
-    op.drop_table('spot_user')
+    op.drop_table("spot_user")
